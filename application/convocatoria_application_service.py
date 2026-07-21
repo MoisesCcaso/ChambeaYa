aeab3c928630e55571693ecda26511dce539caca
MoisesCcaso <mccasoi@unsa.edu.pe>
2026-07-20 21:00:31 -0500

    feat(UC-08): implementar create_convocatoria en ConvocatoriaApplicationService

--- application/convocatoria_application_service.py
+++ application/convocatoria_application_service.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from domain.convocatorias.convocatoria_fabrica import ConvocatoriaFabrica
 
 class ConvocatoriaApplicationService:
-    def __init__(self):
-        pass
+    def __init__(self, convocatoria_repository=None, convocatoria_fabrica=None):
+        self.convocatoria_repository = convocatoria_repository
+        self.convocatoria_fabrica = convocatoria_fabrica or ConvocatoriaFabrica()
 
-    def create_convocatoria(self):
-        pass
+    def create_convocatoria(self, empresa_id, data):
+        self._require_repository()
+        titulo = data.get("titulo")
+        convocatoria = self.convocatoria_fabrica.crear_convocatoria(empresa_id, titulo)
+        return self.convocatoria_repository.save(convocatoria)
 
     def publish(self):
         pass
@@ -16,3 +21,7 @@ class ConvocatoriaApplicationService:
 
     def search(self):
         pass
+
+    def _require_repository(self):
+        if self.convocatoria_repository is None:
+            raise RuntimeError("ConvocatoriaApplicationService requiere un repositorio")
