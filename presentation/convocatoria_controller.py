9a6ff2dd01d942caacad625dbf8525e2a954ae1a
MoisesCcaso <mccasoi@unsa.edu.pe>
2026-07-20 23:08:44 -0500

    feat(UC-08): implementar create en ConvocatoriaController

--- presentation/convocatoria_controller.py
+++ presentation/convocatoria_controller.py
@@ -2,11 +2,13 @@
 # -*- coding: utf-8 -*-
 
 class ConvocatoriaController:
-    def __init__(self):
-        pass
+    def __init__(self, convocatoria_application_service=None):
+        self.convocatoria_application_service = convocatoria_application_service
 
-    def create(self):
-        pass
+    def create(self, empresa_id, payload):
+        self._require_service()
+        convocatoria = self.convocatoria_application_service.create_convocatoria(empresa_id, payload)
+        return self._serialize_convocatoria(convocatoria), 201
 
     def publish(self):
         pass
@@ -16,3 +18,14 @@ class ConvocatoriaController:
 
     def list(self):
         pass
+    def _require_service(self):
+        if self.convocatoria_application_service is None:
+            raise RuntimeError("ConvocatoriaController requiere un application service")
+
+    def _serialize_convocatoria(self, convocatoria):
+        return {
+            "id": convocatoria.id,
+            "empresa_id": convocatoria.empresa_id,
+            "titulo": convocatoria.titulo,
+            "estado": convocatoria.estado,
+        }
