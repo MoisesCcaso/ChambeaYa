30e6aa2d489777d6c239ed7cda2886f6db4cd8fe
MoisesCcaso <mccasoi@unsa.edu.pe>
2026-07-20 21:02:59 -0500

    feat(UC-08): implementar crear_convocatoria en ConvocatoriaFabrica

--- domain/convocatorias/convocatoria_fabrica.py
+++ domain/convocatorias/convocatoria_fabrica.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from domain.convocatorias.convocatoria import Convocatoria
 
 class ConvocatoriaFabrica:
     def __init__(self):
         pass
 
-    def crear_convocatoria(self):
-        pass
+    def crear_convocatoria(self, empresa_id, titulo):
+        if not empresa_id:
+            raise ValueError("La empresa es obligatoria")
+        if not titulo:
+            raise ValueError("El título es obligatorio")
+
+        convocatoria = Convocatoria()
+        convocatoria.empresa_id = empresa_id
+        convocatoria.titulo = titulo
+        convocatoria.estado = Convocatoria.ESTADO_BORRADOR
+        return convocatoria
