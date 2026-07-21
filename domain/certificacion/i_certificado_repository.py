924a9717743009f95ddefe658e007c3a5caed45c
OmarJR18 <oriverao@unsa.edu.pe>
2026-07-21 13:12:21 -0500

    feat: add find_by_practicante_id() to ICertificadoRepository
    Completa las firmas del repositorio y agrega find_by_practicante_id
    para que el practicante liste sus certificados. La implementación
    SQLAlchemy queda pendiente hasta contar con el modelo ORM.

--- domain/certificacion/i_certificado_repository.py
+++ domain/certificacion/i_certificado_repository.py
@@ -6,13 +6,17 @@ from abc import ABC, abstractmethod
 
 class ICertificadoRepository(ABC):
     @abstractmethod
-    def save(self):
+    def save(self, certificado):
         pass
 
     @abstractmethod
-    def find_by_id(self):
+    def find_by_id(self, certificado_id):
         pass
 
     @abstractmethod
-    def find_by_codigo(self):
+    def find_by_codigo(self, codigo):
+        pass
+
+    @abstractmethod
+    def find_by_practicante_id(self, practicante_id):
         pass
--- infrastructure/sqlalchemy_certificado_repository.py
+++ infrastructure/sqlalchemy_certificado_repository.py
@@ -3,16 +3,21 @@
 
 from domain.certificacion.i_certificado_repository import ICertificadoRepository
 
+_NOT_IMPLEMENTED = "Persistencia de certificado pendiente (modelo ORM no disponible)"
+
 
 class SqlAlchemyCertificadoRepository(ICertificadoRepository):
     def __init__(self):
         pass
 
-    def save(self):
-        pass
+    def save(self, certificado):
+        raise NotImplementedError(_NOT_IMPLEMENTED)
 
-    def find_by_id(self):
-        pass
+    def find_by_id(self, certificado_id):
+        raise NotImplementedError(_NOT_IMPLEMENTED)
 
-    def find_by_codigo(self):
-        pass
+    def find_by_codigo(self, codigo):
+        raise NotImplementedError(_NOT_IMPLEMENTED)
+
+    def find_by_practicante_id(self, practicante_id):
+        raise NotImplementedError(_NOT_IMPLEMENTED)
