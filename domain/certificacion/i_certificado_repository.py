26a9e878e46ee27748bb28e8989f9c7a2c7d5022
Torres Chavez Jhair <jtorreschav@unsa.edu.pe>
2026-07-23 06:57:24 -0500

    feat(UC-22): agregar find_by_practica_id a ICertificadoRepository

--- domain/certificacion/i_certificado_repository.py
+++ domain/certificacion/i_certificado_repository.py
@@ -1,18 +1,19 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
 from abc import ABC, abstractmethod
 
 
 class ICertificadoRepository(ABC):
     @abstractmethod
-    def save(self):
+    def save(self, certificado):
         pass
 
     @abstractmethod
-    def find_by_id(self):
+    def find_by_id(self, certificado_id):
         pass
 
     @abstractmethod
-    def find_by_codigo(self):
+    def find_by_codigo(self, codigo):
+        pass
+
+    @abstractmethod
+    def find_by_practica_id(self, practica_id):
         pass
