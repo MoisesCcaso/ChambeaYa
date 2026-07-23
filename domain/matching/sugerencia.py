1b0f11e0a36d8c28e7bccc2d47cac6b37506e02e
Torres Chavez Jhair <jtorreschav@unsa.edu.pe>
2026-07-23 09:33:31 -0500

    feat(UC-12): implementar entidades de dominio Sugerencia y ResultadoMatching

--- domain/matching/sugerencia.py
+++ domain/matching/sugerencia.py
@@ -1,9 +1,21 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
 class Sugerencia:
-    def __init__(self):
-        self.id = None
-        self.practicante_id = None
-        self.convocatoria_id = None
-        self.puntaje_match = None
+    def __init__(self, id=None, practicante_id=None, convocatoria_id=None,
+                 puntaje_match=None, habilidades_coincidentes=None):
+        self.id = id
+        self.practicante_id = practicante_id
+        self.convocatoria_id = convocatoria_id
+        self.puntaje_match = puntaje_match or 0.0
+        self.habilidades_coincidentes = habilidades_coincidentes or []
+
+    def calcular_compatibilidad(self, habilidades_practicante, habilidades_requeridas):
+        if not habilidades_requeridas:
+            self.puntaje_match = 0.0
+            return self.puntaje_match
+
+        set_practicante = set(h for h in habilidades_practicante if h)
+        set_requeridas = set(h for h in habilidades_requeridas if h)
+        coincidentes = set_practicante & set_requeridas
+
+        self.habilidades_coincidentes = list(coincidentes)
+        self.puntaje_match = len(coincidentes) / len(set_requeridas) * 100
+        return self.puntaje_match
