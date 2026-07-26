995072b869ecd73348b031cd175e743d42f96963
MoisesCcaso <mccasoi@unsa.edu.pe>
2026-07-26 14:05:18 -0500

    feat(UC-24): registrar certificado_bp en register_routes

--- frameworks/flask_mvc/routes/__init__.py
+++ frameworks/flask_mvc/routes/__init__.py
@@ -4,6 +4,7 @@ from frameworks.flask_mvc.routes.perfil_routes import perfil_bp
 from frameworks.flask_mvc.routes.practica_routes import practica_bp
 from frameworks.flask_mvc.routes.convocatoria_routes import convocatoria_bp
 from frameworks.flask_mvc.routes.postulacion_routes import postulacion_bp
+from frameworks.flask_mvc.routes.certificados_routes import certificado_bp
 
 def register_routes(app):
     app.register_blueprint(auth_bp)
@@ -12,3 +13,4 @@ def register_routes(app):
     app.register_blueprint(health_bp)
     app.register_blueprint(perfil_bp)
     app.register_blueprint(practica_bp)
+    app.register_blueprint(certificado_bp)
