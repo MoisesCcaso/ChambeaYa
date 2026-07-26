e4ff0448efc93931c74c32ce22403d8b11182e5d
MoisesCcaso <mccasoi@unsa.edu.pe>
2026-07-26 12:03:06 -0500

    feat(UC-24): implementar generar_pdf y verificar_integridad en Certificado

--- domain/certificacion/certificado.py
+++ domain/certificacion/certificado.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from domain.certificacion.archivo_pdf import ArchivoPDF
 
 class Certificado:
-    def __init__(self):
-        self.id = None
-        self.practica_id = None
-        self.codigo_qr = None
-        self.documento = None
+    def __init__(self, id=None, practica_id=None, codigo_qr=None, documento=None):
+        self.id = id
+        self.practica_id = practica_id
+        self.codigo_qr = codigo_qr
+        self.documento = documento
 
-    def generar_pdf(self):
-        pass
+    def generar_pdf(self, contenido):
+        self.documento = ArchivoPDF.crear(self.practica_id, contenido)
+        return self.documento
 
-    def verificar_integridad(self):
-        pass
+    def verificar_integridad(self, contenido):
+        if self.documento is None:
+            return False
+        return self.documento.verificar_integridad(contenido)
